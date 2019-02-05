245115eaa0d88e7ebed8ac85e0db3e578542ccdd
Mark Matas <46798532+markm700@users.noreply.github.com>
2019-02-05 14:43:16 -0700

    created group.c file

---
+++ group.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void member1(void){
+
+}
+
+void member2(void){
+
+}
+
+void member3(void){
+
+}
+
+void member4(void){
+
+}
+
+void member5(void){
+
+}
+
+int main(void){
+	printf("**************************\n");
+	printf("Our group is called: \n");
+	printf("Our members: \n");
+	printf("**************************\n");
+	
+	member1();
+	member2();
+	member3();
+	member4();
+	member5();
+	
+	return 0;
+}
